caf37ebb7ecd4475192a8f891ca93c87bb017cd4
fuston05 <fuston05@yahoo.com>
2020-01-27 15:04:35 -0500

    cleaned up code a little, made sure all project was completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
-   <div>
-      <Navigation />
-      <Route path= '/' exact component= {Home}>
-         {/* <Home /> */}
-      </Route>
-      <Route path= '/about' component= {About}>
-         {/* <About /> */}
-      </Route>
-      <Route path= '/contact' component= {Contact}>
-         {/* <Contact /> */}
-      </Route>
-   </div>
+  <div>
+    <Navigation />
+    <Route path='/' exact component={Home} />
+    <Route path='/about' component={About} />
+    <Route path='/contact' component={Contact} />
+  </div>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-   <Router>
-      <App />
-   </Router>
-   , document.getElementById('root'));
+  <Router>
+    <App />
+  </Router>
+  , document.getElementById('root'));
